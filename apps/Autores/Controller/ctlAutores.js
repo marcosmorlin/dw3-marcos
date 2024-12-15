b15f9c093410c00132cfdebb750ac24a7a55bf80
marcosmorlin <145074070+marcosmorlin@users.noreply.github.com>
2024-12-15 19:04:22 -0300

    Add files via upload

---
+++ apps/Autores/Controller/ctlAutores.js
@@ -0,0 +1,46 @@
+const mdlCursos = require("../model/mdlAutores");
+
+const GetAllAutores = (req, res) =>
+    (async () => {
+      let registro = await mdlAutores.GetAllCursos();
+      res.json({ status: "ok", registro: registro });
+    })();
+
+const GetAutorByID = (req, res) =>
+  (async () => {
+    const ID = parseInt(req.body.cursoid);
+    let registro = await mdlAutores.GetAutorByID(ID);
+
+    res.json({ status: "ok", registro: registro });
+  })();
+  
+const InsertAutores = (request, res) =>
+  (async () => {
+    //@ Atenção: aqui já começamos a utilizar a variável msg para retornar erros de banco de dados.
+    const registro = request.body;
+    let { msg, linhasAfetadas } = await mdlAutores.InsertAutores(registro);
+    res.json({ status: msg, linhasAfetadas: linhasAfetadas });
+  })();
+
+const UpdateAutores = (request, res) =>
+  (async () => {
+    const registro = request.body;
+    let { msg, linhasAfetadas } = await mdlAutores.UpdateAutores(registro);
+    res.json({ status: msg, linhasAfetadas: linhasAfetadas });
+  })();
+
+const DeleteAutores = (request, res) =>
+  (async () => {
+    const registro = request.body;
+    let { msg, linhasAfetadas } = await mdlAutores.DeleteAutores(registro);
+    res.json({ status: msg, linhasAfetadas: linhasAfetadas });
+  })();
+  
+module.exports = {
+  GetAllAutores,
+  GetAutorByID,
+  InsertAutores,
+  UpdateAutores,
+  DeleteAutores
+};
+  
